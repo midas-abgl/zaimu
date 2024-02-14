b84faafb716f9e8527d1b3cd9f9cd061277e29d0
Aran Leite <hyoretsu@gmail.com>
2024-02-14 17:18:36 -0300

    Update favicons and theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,9 @@ import { PropsWithChildren } from "react";
 import "./_global.scss";
 import WebVitals from "./components/WebVitals";
 
-const url = "https://zaimu.me";
-
-const siteColor = "#1e5a70";
+const siteColor = "#4F53B7";
 const siteName = "Zaimu";
+const url = "https://zaimu.me";
 
 export const metadata = {
 	applicationName: siteName,
@@ -42,23 +41,9 @@ export default function RootLayout({ children }: PropsWithChildren) {
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<head>
-				<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png" />
-				<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png" />
-				<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png" />
-				<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png" />
-				<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png" />
-				<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png" />
-				<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png" />
-				<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png" />
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png" />
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-chrome-192x192.png" />
-				<link rel="icon" type="image/png" sizes="194x194" href="/favicons/favicon-194x194.png" />
+				<link rel="icon" type="image/svg+xml" href="favicon.svg" />
+				<link rel="icon" type="image/png" href="favicon.png" />
 				<link rel="manifest" href="/site.webmanifest" />
-				<link rel="mask-icon" href="/safari-pinned-tab.svg" color={siteColor} />
-				<meta name="msapplication-TileColor" content={siteColor} />
-				<meta name="msapplication-TileImage" content="/mstile-144x144.png" />
 				<meta name="theme-color" content={siteColor} />
 			</head>
 			<body>
