e17d2a778c5aa6d0960d9dcacd8189bd61fb9560
Breno-Lima <breno.souza@academico.ufpb.br>
2024-03-10 22:29:02 -0300

    Added fake accounts repository

---
+++ backend/src/modules/accounts/repositories/fakes/accounts.repository.ts
@@ -0,0 +1,23 @@
+import { randomUUID } from "node:crypto";
+import type { Account } from "@database/types";
+import type { CreateAccountDTO } from "@modules/accounts/dtos/CreateAccount.dto";
+import { Injectable } from "@nestjs/common";
+import type AccountsRepository from "../accounts.repository";
+
+@Injectable()
+export default class FakeAccountsRepository implements AccountsRepository {
+	private accounts: Account[] = [];
+
+	public async create(data: CreateAccountDTO): Promise<Account> {
+		const account = {
+			...data,
+			id: randomUUID(),
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		} as unknown as Account;
+
+		this.accounts.push(account);
+
+		return account;
+	}
+}
