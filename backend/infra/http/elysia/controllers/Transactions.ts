8197caf56320e88057d0e24d69fe2d66b26fb00a
Aran Leite <hyoretsu@gmail.com>
2024-07-06 03:00:17 -0300

    Backend: added transaction recurrence
    closes #5

--- backend/infra/http/elysia/controllers/Transactions.ts
+++ backend/infra/http/elysia/controllers/Transactions.ts
@@ -18,11 +18,13 @@ export const TransactionsController = new Elysia()
 			.post("/", ({ body, addTransaction }) => addTransaction.execute(body), {
 				body: t.Object({
 					amount: t.Number({ minimum: 0 }),
+					categories: t.Optional(t.Array(t.String({ maxLength: 50 }))),
 					date: t.Date(),
 					description: t.Optional(t.String({ maxLength: 1000 })),
-					categories: t.Optional(t.Array(t.String({ maxLength: 50 }))),
 					destinationId: t.Optional(t.String({ pattern: uuidRegex.source })),
 					originId: t.Optional(t.String({ pattern: uuidRegex.source })),
+					recurrence: t.Optional(t.String({ maxLength: 26 })),
+					repeatCount: t.Optional(t.Number({ min: 2 })),
 				}),
 				response: t.Object({
 					id: t.String(),
@@ -30,6 +32,8 @@ export const TransactionsController = new Elysia()
 					date: t.Date(),
 					description: t.Nullable(t.String()),
 					categories: t.Array(t.String()),
+					recurrence: t.Nullable(t.String()),
+					repeactCount: t.Nullable(t.Number()),
 					destinationId: t.Nullable(t.String()),
 					originId: t.Nullable(t.String()),
 					createdAt: t.Date(),
