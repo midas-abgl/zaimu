a49982da1f35769c321f1819743e2507ff217160
Aran Leite <hyoretsu@gmail.com>
2024-07-07 15:13:25 -0300

    Fix RootLayout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,8 +1,41 @@
 import type { PropsWithChildren } from "react";
 import "./_global.scss";
-import { viewport } from "../metadata";
 import WebVitals from "./components/WebVitals";
 import Sidebar from "./components/sidebar/SideBar";
+
+const siteName = "Zaimu";
+const url = "https://zaimu.me";
+
+export const metadata = {
+	applicationName: siteName,
+	appleWebApp: {
+		title: siteName,
+	},
+	metadataBase: new URL(url),
+	openGraph: {
+		images: [
+			{
+				url: "/opengraph.jpg",
+				width: 1200,
+				height: 627,
+				alt: siteName,
+			},
+		],
+		siteName,
+		type: "website",
+	},
+	title: {
+		default: siteName,
+		template: `%s | ${siteName}`,
+	},
+	twitter: {
+		card: "summary_large_image",
+		creator: `${process.env.NEXT_PUBLIC_SITE_CONTENT_CREATOR}` || "@hyoretsu",
+	},
+};
+export const viewport = {
+	themeColor: "#4F53B7",
+};
 
 export default function RootLayout({ children }: PropsWithChildren) {
 	return (
